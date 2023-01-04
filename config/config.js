670031c9b7a3b1c80730a47ba3009deeeb39db13
01057131 <116507892+01057131@users.noreply.github.com>
2023-01-04 18:41:54 +0800

    Add files via upload

---
+++ config/config.js
@@ -0,0 +1,10 @@
+const config = {
+  app: {
+    port: 3000,
+  },
+  db: {
+    url: "mongodb+srv://01057131:test01057131@cluster0.lhe3jdf.mongodb.net/?retryWrites=true&w=majority",
+  },
+  //"mongodb+srv://say859462:test001@cluster0.n4rhnlu.mongodb.net/?retryWrites=true&w=majority"
+};
+module.exports = config;
